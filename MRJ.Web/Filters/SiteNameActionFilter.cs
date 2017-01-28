175a943dee2f731bddd9f10b96efdf7b6fc31939
mrajabpour <Rajabpoor85.iteng@gmail.com>
2017-01-28 22:29:41 +0330

    Add files via upload

---
+++ MRJ.Web/Filters/SiteNameActionFilter.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using MRJ.ServiceLayer.Contracts;
+using MRJ.Web.Caching;
+using MRJ.Web.DependencyResolution;
+
+namespace MRJ.Web.Filters
+{
+    public class SiteNameActionFilter : ActionFilterAttribute
+    {
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            var siteSettings = IoC.Container.GetInstance<ICacheService>().GetSiteSettings();
+
+            filterContext.Controller.ViewBag.SiteName = siteSettings.SiteName;
+            filterContext.Controller.ViewBag.ContactInfo = siteSettings.ContactInfo;
+        }
+
+    }
+}
